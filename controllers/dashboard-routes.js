0bc035aef781979665dd511fff44395fd008be19
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-02-18 14:19:47 -0800

    got to listen

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,10 +1,97 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+router.get('/', withAuth, (req, res) => {
+
+    Post.findAll({
+        //use id from session
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            //serialize the data before passing to template
+            const posts = dbPostData.map(post => post.get({ plain: true }))
+            res.render('dashboard', { posts, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// create route to edit a post
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            //serialize the data
+            const post = dbPostData.get({ plain: true });
+
+            // pass data to template
+            res.render('edit-post', {
+                post,
+                loggedIn: true
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+})
+
+// create route for new post page
+router.get('/newpost', withAuth, (req, res) => {
+    res.render('new-post', { loggedIn: true });
+})
 
 module.exports = router;
