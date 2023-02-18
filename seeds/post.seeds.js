bcbb1d7ef23bba8833739c0beb19a55572ab9164
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-02-18 11:13:13 -0800

    added seeds

--- seeds/post.seeds.js
+++ seeds/post.seeds.js
@@ -1,7 +1,19 @@
 const { Post } = require('../models');
 
 const postdata = [
-{}
+{
+    title: 'Why MVC is so important',
+    post_content:'MVC allows developers to maintain a true separation of concerns, devising their code between the Model ayer for data, the View layer for design, and the Controller layer for application logic.'
+},
+{
+    title: 'Authentication vs Authorization',
+    post_content:"Ther is a difference between authentication and authoirzation.  Authentication eans confirming your own identitiy, whereas authorization meas being allowed access to the syste,"
+},
+{
+    title: "Object-Relation Mapping",
+    post_content: "I have really loved learning about ORMS. It's really simplified the way I create queries in SQL"
+}
+
 ];
 
 const seedPosts = () => Post.bulkCreate(postdata);
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,8 +2,33 @@ const sequelize = require('../config/connection');
 const { User, Post } = require('../models');
 
 const userdata = [
- {},
-];
+    {
+      name: "Sal",
+      email: "sal@hotmail.com",
+      password: "password12345"
+    },
+    {
+      name: "Lernantino",
+      email: "lernantino@gmail.com",
+      password: "password12345"
+    },
+    {
+      name: "Amiko",
+      email: "amiko2k20@aol.com",
+      password: "password12345"
+    },
+    {
+      name: "Jordan",
+      email: "jordan99@msn.com",
+      password: "password12345"
+    },
+    {
+      name: "Blake",
+      email: "the_blake@yahoo.com",
+      password: "password12345"
+    }
+  ]
+  
 
 const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
 
