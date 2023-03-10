42b976843b4383f943ccf3340907949ca78e193d
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-10 12:10:20 -0800

    fixed css and fixed the dashboard to have the user click and edit page shows up

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -70,7 +70,7 @@ router.get('/edit/:id', withAuth, (req, res) => {
     })
         .then(dbPostData => {
             if (!dbPostData) {
-                res.status(404).json({ message: 'No post found with this id' });
+                res.status(404).json({ message: 'No id' });
                 return;
             }
 
