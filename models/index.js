8dd1c849bfa15e0f70cf2c71538d06be85eef492
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-03-10 14:19:28 -0800

    change caps on models

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // take in other files to use models
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
 
 //create tieins
 User.hasMany(Post, {
